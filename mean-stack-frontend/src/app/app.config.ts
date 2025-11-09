b1e511becfcd21722dd0b956fa8dd9d5bbfd77a8
mlm57-matt <mlm57@njit.edu>
2025-11-09 14:14:55 -0500

    Express.js set up. Generated service api

--- mean-stack-frontend/src/app/app.config.ts
+++ mean-stack-frontend/src/app/app.config.ts
@@ -1,3 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'; // ✅ Import HttpClientModule
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule // ✅ Add here
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
